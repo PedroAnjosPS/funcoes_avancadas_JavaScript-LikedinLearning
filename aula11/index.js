9caa631b34344b1b875fbab8b3446a0fad358f2a
PedroAnjosPS <phpsanches@gmail.com>
2025-10-05 00:42:34 -0300

    aula sobre Strict Mode

---
+++ aula11/index.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const myFunc = () => {
+    let nome = "Pedro";
+
+    console.log(`Olá, ${nome}`);
+};
+
+myFunc();
+
+//console.log(nome);
+
+function calculador(x, y) {
+    console.log(x + y);
+}
+
+calculador(5, 10);
